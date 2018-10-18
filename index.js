bc75d67b6a396374101f59cf0cf1eda408b9b40f
Damien Reimert <damien.reimert@gmail.com>
2018-10-18 14:54:06 +0200

    Factorisation code pour plus de lisibilité

--- index.js
+++ index.js
@@ -3,25 +3,31 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 
 // Télécharger une page HTML
-const htmlPromise = rp('https://www.insa-lyon.fr/fr/formation/parcours/729/4/1')
-.then(function (htmlString) {
-  // console.log(htmlString);
-  return htmlString;
-})
-.catch(function (err) {
-  console.error("RP ERROR:", err)
-});
+const getHtml = (url) => {
+  return rp(url)
+  .then(function (htmlString) {
+    // console.log(htmlString);
+    return htmlString;
+  })
+  .catch(function (err) {
+    console.error("RP ERROR:", err)
+    return Promise.reject(err);
+  });
+}
 
 // Télécharger un pdf
-const pdfPromise = rp({
-  url: 'http://planete.insa-lyon.fr/scolpeda/f/ects?id=36736&_lang=fr',
-  encoding: null
-}).then(function (pdf) {
-  // console.log("PDF download");
-  return pdf;
-}).catch(function (err) {
-  console.error("RP ERROR:", err)
-});
+const getPdf = (url) => {
+  return rp({
+    url: url,
+    encoding: null
+  }).then(function (pdf) {
+    // console.log("PDF download");
+    return pdf;
+  }).catch(function (err) {
+    console.error("RP ERROR:", err)
+    return Promise.reject(err);
+  });
+}
 
 // Lance le serveur Tika
 // Doc : https://www.npmjs.com/package/tika-server
@@ -33,12 +39,14 @@ ts.on("debug", (msg) => {
 
 // Lance le serveur tika
 ts.start().then(() => {
-  // Pour tous les éléments du tableau
-  const listDePdfPromise = [pdfPromise];
-  // Pour chaque pdf, on fait des trucs
-  return Promise.all(listDePdfPromise.map((elem) => {
+  // liste de mes urls de pdf
+  const listeUrlPdfs = [
+    'http://planete.insa-lyon.fr/scolpeda/f/ects?id=36736&_lang=fr'
+  ]
+  // Pour chaque url ...
+  return Promise.all(listeUrlPdfs.map((url) => {
     // Extraction du texte.
-    return elem.then((pdf) => {
+    return getPdf(url).then((pdf) => {
       // console.log("pdf", pdf);
       return ts.queryText(pdf).then((data) => {
         // console.log(data)
@@ -56,7 +64,7 @@ ts.start().then(() => {
 // Analyser du html
 // DOc : https://github.com/cheeriojs/cheerio
 // ou encore : https://github.com/sfrenot/competence/blob/master/formation/crawl.coffee
-htmlPromise.then((html) => {
+getHtml('https://www.insa-lyon.fr/fr/formation/parcours/729/4/1').then((html) => {
   const $ = cheerio.load(html);
   const urls = $('#block-system-main .content-offre-formations table a').map(function() {
     return $(this).attr('href');
@@ -70,7 +78,7 @@ const db = {}
 // Écrire dans la base de données
 
 db["code"] = {
-  a: 1
+  a: 1,
   b: 2
 }
 
