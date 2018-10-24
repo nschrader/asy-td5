3ccf7997015b099ee8ea821124a181c3e4031cfb
Schrader Nick <nschrader@tc405-020-12.insa-lyon.fr>
2018-10-24 19:27:19 +0200

    working until pdf scaping using regex

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const TikaServer = require("tika-server");
-const rp = require('request-promise');
-const cheerio = require('cheerio');
+const rp = require('request-promise')
+const Promise = require('bluebird')
+const TikaServer = require("tika-server")
+const cheerio = require('cheerio')
 
 // Télécharger une page HTML
 const getHtml = (url) => {
@@ -29,18 +30,59 @@ const getPdf = (url) => {
   });
 }
 
+// Analyser du html
+// DOc : https://github.com/cheeriojs/cheerio
+// ou encore : https://github.com/sfrenot/competence/blob/master/formation/crawl.coffee
+const tableSelector = '#block-system-main .content-offre-formations table a'
+const crawlHtmlForUrls = (url) => {
+  return new Promise((resolve) => {
+    getHtml(url).then((html) => {
+      const $ = cheerio.load(html)
+      const urls = $(tableSelector).map((i, e) => $(e).attr('href'))
+      resolve(urls.get())
+    })
+  })
+}
+
+const host = 'https://www.insa-lyon.fr'
+var semesters = crawlHtmlForUrls(host + '/fr/formation/diplomes/ING')
+
+var units = Promise.map(semesters, (s) => crawlHtmlForUrls(host + s))
+var pdfs = new Promise((resolve) => {
+  units.then((urls) => {
+    var flat = [].concat(...urls)
+    resolve([...new Set(flat)])
+  })
+})
+
 // Lance le serveur Tika
 // Doc : https://www.npmjs.com/package/tika-server
 const ts = new TikaServer();
 
 ts.on("debug", (msg) => {
-  // console.log(`DEBUG: ${msg}`)
+  //console.log(`DEBUG: ${msg}`)
 })
 
 // Lance le serveur tika
 ts.start().then(() => {
+  pdfs.map((url) =>  {
+    // Extraction du texte.
+    return getPdf(url).then((pdf) => {
+      // console.log("pdf", pdf);
+      return ts.queryText(pdf).then((data) => {
+        // console.log(data)
+        let code = /CODE : ([^\n]*)/.exec(data)[1];
+        console.log("Code :", code);
+      });
+    })
+  }).then(() => {
+    return ts.stop()
+  }).catch((err) => {
+    console.log(`TIKA ERROR: ${err}`)
+  })
+})
   // liste de mes urls de pdf
-  const listeUrlPdfs = [
+  /*const listeUrlPdfs = [
     'http://planete.insa-lyon.fr/scolpeda/f/ects?id=36736&_lang=fr'
   ]
   // Pour chaque url ...
@@ -59,41 +101,10 @@ ts.start().then(() => {
   return ts.stop()
 }).catch((err) => {
   console.log(`TIKA ERROR: ${err}`)
-})
+})*/
 
-// Analyser du html
-// DOc : https://github.com/cheeriojs/cheerio
-// ou encore : https://github.com/sfrenot/competence/blob/master/formation/crawl.coffee
-const crawlHtmlForUrls = (url) => {
-  return new Promise((resolve) => {
-    getHtml(url).then((html) => {
-      const $ = cheerio.load(html)
-      const urls = $('#block-system-main .content-offre-formations table a').map(function() {
-        return $(this).attr('href')
-      })
-      resolve(urls.get())
-    })
-  })
-}
-
-var semesters = crawlHtmlForUrls('https://www.insa-lyon.fr/fr/formation/diplomes/ING')
-
-var ects = new Promise((resolve) => {
-  semesters.then((urls) => {
-    var withHost = urls.map((url) => 'https://www.insa-lyon.fr' + url)
-    //console.log("semesters:", withHost)
-    var arraysInArrays = withHost.map((url) => crawlHtmlForUrls(url))
-    //arraysInArrays.forEach((x) => x.then((y) => console.log(y)))
-    /*withHost.forEach((url) => {
-      crawlHtmlForUrls(url).then((x) => console.log(x))
-    })*/
-    resolve(Array.concat(arraysInArrays))
-  })
-})
-//semesters.then((urls) => console.log("semesters:", urls))
-
-//var ects = crawlHtmlForUrls('https://www.insa-lyon.fr/fr/formation/parcours/729/4/1')
-ects.then((urls) => console.log("ects:", urls))
+//romise.all(ects).then((urls) => console.log("ects:", urls)).catch((urls) => console.log('Cannot get' + urls))
+//ects.then((urls) => console.log("ects:", urls))
 
 
 
